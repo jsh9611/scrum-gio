8d5a36b0f6e1664cbe40de555e580fcaf0030f92
jsh9611 <jangsh9611@naver.com>
2022-10-11 23:11:41 +0900

    [Add] project file

---
+++ FirstScrum/FirstScrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstScrumApp.swift
+//  FirstScrum
+//
+//  Created by SeongHoon Jang on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct FirstScrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
